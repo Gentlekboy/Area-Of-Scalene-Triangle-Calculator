e2ea013bbba97f5f8876ba461d1b3d3c61d8c74e
Kufre Udoh <udoh.kufre.m@gmail.com>
2020-07-08 13:44:07 +0100

    Updated The Function To Run The Calculation

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
+
+// TO GET VALUES
+let firstSide = document.getElementById("firstSide");
+let secondSide = document.getElementById("secondSide");
+let thirdSide = document.getElementById("thirdSide");
+let result = document.getElementById("result");
+let button = document.getElementById("btn");
+
 function calArea() {
-  const firstSide = parseFloat(document.getElementById("firstSide").value);
-  const secondSide = parseFloat(document.getElementById("secondSide").value);
-  const thirdSide = parseFloat(document.getElementById("thirdSide").value);
-  const result = document.getElementsByClassName("result");
+  // APPLYING THE FORMULA
+  let halfPerimeter = (firstSide.value + secondSide.value + thirdSide.value)/2;
+  let area = Math.sqrt(halfPerimeter*(halfPerimeter - firstSide.value)*(halfPerimeter - secondSide.value)*(halfPerimeter - thirdSide.value));
 
-  let halfPerimeter = (firstSide + secondSide + thirdSide)/2;
+  // TO MAKE SURE RESULT IS ROUNDED OFF
+  function roundFig(number) {
+    return Math.round(number * 100)/100;
+  }
 
-  let area = Math.sqrt(halfPerimeter*(halfPerimeter - firstSide)*(halfPerimeter - secondSide)*(halfPerimeter - thirdSide));
+  result.innerHTML = "The Half Perimeter Of The Circle Is " + roundFig(halfPerimeter) + " And The Area Is " + roundFig(area) + " .";
 
-  let resultMessage = 'The area of the scalene triangle is ' + area;
-
-  result.innerText = resultMessage;
-
-  document.getElementById("btn").addEventListener("click", result.innerText);
+  console.log(halfPerimeter);
+  console.log(area);
+  console.log(thirdSide.value);
+  console.log(secondSide.value);
+  console.log(firstSide.value);
 }
-
+button.addEventListener(onclick, calArea);
